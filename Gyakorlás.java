56dfee9e446af5868c6d19d7e7d11955eee5ef96
Tóth Alexandra <118132673+TAlexa1997@users.noreply.github.com>
2023-02-23 18:34:15 +0100

    kész

---
+++ Gyakorlás.java
@@ -0,0 +1,36 @@
+
+package gyakorlás;
+
+
+public class Gyakorlás {
+
+  
+    public static void main(String[] args) {
+        int osszeg = elso10SzamOsszege();
+
+        kiir("Az első 10 szám összege: " + osszeg);
+        int[] szamok = new int[]{3, 7, 10};
+        osszeg = osszead(szamok);
+        kiir("A megadott számok összege: %d\n".formatted(osszeg));
+    }
+
+    private static int elso10SzamOsszege() {
+        int osszeg = 0;
+        for (int i = 0; i < 10; i++) {
+            osszeg += i;
+        }
+        return osszeg;
+    }
+
+    private static int osszead(int[] szamok) {
+        int osszeg = 0;
+        for (int szam: szamok) {
+            osszeg += szam;
+        }
+        return osszeg;
+    }
+
+    private static void kiir(String szoveg) {
+        System.out.println(szoveg);
+    }
+}
